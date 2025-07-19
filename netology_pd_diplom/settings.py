c64c3b23760454b4df879cd4dd0fa0d9fecad27e
venche1980 <venche1980@gmail.com>
2025-07-19 21:58:58 +0300

    feat: завершена настройка проекта. Настроено подключение к PostgreSQL. Созданы миграции для backend. Применены все миграции.

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diplom_db',
-        'USER': 'diplom_user',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
